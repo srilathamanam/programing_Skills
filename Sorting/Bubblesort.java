bb17a01cb0869afb4a16a89647f540552b9b6a30
srilathamanam <srilatha.manam@gmail.com>
2022-08-08 10:28:31 +0530

    Update Bubblesort.java

--- Sorting/Bubblesort.java
+++ Sorting/Bubblesort.java
@@ -22,13 +22,13 @@ class Bubblesort
         } 
         for (int i=0; i<n-1; i++)       
         {
-        for(int j = i+1; j < n; j++)    
+        for(int j = 0; j < n-1; j++)    
           {
-            if (arr[i] > arr[j])      
+            if (arr[j] > arr[j+1])      
             {                           
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
           }
     }
