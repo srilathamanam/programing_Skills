38fafe149a60dc09094898c4df8cfa22f06127fe
srilathamanam <srilatha.manam@gmail.com>
2023-02-14 15:49:38 +0530

    Add files via upload

---
+++ Test/Hello.java
@@ -0,0 +1,22 @@
+
+// logical operators
+// &&,||,!
+// more than one relational expresssion we have to go for logical operators
+
+public class Hello {
+  public static void main(String ar[]) {
+    boolean a, b;
+    // increment operator ++,decrement operator --
+    // postfix notation, prefix notation
+    a = true;
+   
+   if(a)
+   {
+     System.out.println("a is a +ve no");
+     System.out.println(a);
+   }
+    else
+     System.out.println("a is not a +ve no");
+
+  }
+}
